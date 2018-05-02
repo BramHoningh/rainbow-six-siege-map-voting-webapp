c640aff1fff4a308134c32869cbf3a2b95f5c911
Pixeldenker <bram@pixeldenker.nl>
2018-05-02 14:15:38 +0200

    Add socket.io

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,20 @@
 </template>
 
 <script>
+import io from 'socket.io-client'
+import serverLocation from '../assets/server'
+
 // @ is an alias to /src
 
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+
+    }
+  },
+  created () {
+    const socket = io(serverLocation)
+  }
 };
 </script>
